c864e0fc2374ef24ac366b3efd9249bc814af69e
Jeong Pak <dannypak716@gmail.com>
2021-09-29 16:20:44 -0400

    i THINK i completed step 2...

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,6 +2,16 @@ import React, {useState} from 'react';
 import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = (initialValues) => {
-    const [values, setValues] = useLocalStorage("mode", initialValues)
-    return();
+    const [values, setValues] = useLocalStorage("mode")
+
+    const handleChanges = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    return [values, handleChanges];
 }
+
+export default useDarkMode;
